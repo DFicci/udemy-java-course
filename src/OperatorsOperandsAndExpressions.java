1a1c4eebfd159dd651d21e0beeabf122e6828139
daniel ficci <danielficci@gmail.com>
2022-03-10 10:17:50 -0600

    fixed operator challenge

--- src/OperatorsOperandsAndExpressions.java
+++ src/OperatorsOperandsAndExpressions.java
@@ -96,7 +96,7 @@ public class OperatorsOperandsAndExpressions {
 
         double myFirstValue = 20.00d;
         double mySecondValue = 80.00d;
-        double myValuesTotal = myFirstValue + mySecondValue * 100.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
         System.out.println("MyValuesTotal = " + myValuesTotal);
         double theRemainder = myValuesTotal % 40.00d;
         System.out.println("theRemainder = " + theRemainder);
