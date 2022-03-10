ba01a0a777673e73bb933e1717e7df1569b4a836
daniel ficci <danielficci@gmail.com>
2022-03-10 10:16:02 -0600

    operator challenge

--- src/OperatorsOperandsAndExpressions.java
+++ src/OperatorsOperandsAndExpressions.java
@@ -90,5 +90,22 @@ public class OperatorsOperandsAndExpressions {
         if (wasCar){
             System.out.println("wasCar is true");
         }
+
+
+        //Continued: Operator Challenge---------------------------------------------------------------------------------
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = myFirstValue + mySecondValue * 100.00d;
+        System.out.println("MyValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+        if (!isNoRemainder){
+            System.out.println("got some remainder.");
+        }
+
+
     }
 }
